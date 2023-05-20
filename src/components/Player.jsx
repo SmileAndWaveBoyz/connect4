3a303893a9c7eb8ac0cc5cbe371e220ec6a91df6
Nathan Caney <nathancaney@gmail.com>
2023-05-20 17:45:43 +0100

    Game page

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,9 +3,90 @@ import {Route, Routes, useLocation } from 'react-router-dom';
 
 function Player() {
   return (
-    <div className="buttonsContainer">
-        <img src="./assets/images/logo.svg" alt="logo" className='logoImage'/>
+    <div className="playArea">
+      <div className="playButtonsContainer">
+          <button className='gameButton'>MENUE</button>
+          <img className="playScreenLogo" src="./assets/images/logo.svg" alt="logo"/>
+          <button className='gameButton'>RESTART</button>
+      </div>
+      <div className="playersBox">
+        <div className="playersTextBubble">
+          <h2>PLAYER 1</h2>
+          <p>12</p>
+          <img className='playerOneImage' src="./assets/images/player-one.svg" alt="Player 1 smiley face" />
+        </div>
+        <div className="playersTextBubble">
+          <h2>PLAYER 1</h2>
+          <p>12</p>
+          <img className='playerTwoImage' src="./assets/images/player-two.svg" alt="Player 2 smiley face" />
+        </div>
+      </div>
 
+      <div className="gameBoard">
+        <img className='backImageLayer' src="/assets/images/board-layer-black-small.svg" alt="" />
+        <table>
+          <tr>
+            <td><img src="/assets/images/counter-red-small.svg" alt="" /></td>
+            <td><img src="/assets/images/counter-red-small.svg" alt="" /></td>
+            <td><img src="/assets/images/counter-red-small.svg" alt="" /></td>
+            <td><img src="/assets/images/counter-red-small.svg" alt="" /></td>
+            <td><img src="/assets/images/counter-red-small.svg" alt="" /></td>
+            <td><img src="/assets/images/counter-red-small.svg" alt="" /></td>
+            <td><img src="/assets/images/counter-red-small.svg" alt="" /></td>
+          </tr>
+          <tr>
+            <td><img src="/assets/images/counter-red-small.svg" alt="" /></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td><img src="/assets/images/counter-red-small.svg" alt="" /></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td><img src="/assets/images/counter-red-small.svg" alt="" /></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td><img src="/assets/images/counter-red-small.svg" alt="" /></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td><img src="/assets/images/counter-red-small.svg" alt="" /></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </table>
+        <div className="frontImageLayer"></div>
+        
+        <div className="playerTurnBox">
+            <h3>PLAYER 1'S TURN</h3>
+            <p>15s</p>
+          </div>
+      </div>
     </div>
   )
 }
