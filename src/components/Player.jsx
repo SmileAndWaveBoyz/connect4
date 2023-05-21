46fdc2446b6bd630bf61b1229f47f9d5b6299704
Nathan Caney <nathancaney@gmail.com>
2023-05-21 18:32:15 +0100

    Table logic loop

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import {Route, Routes, useLocation } from 'react-router-dom';
 
 function Player() {
+  //Table logic
+  const[column0, setcolumn0] = useState([0, 0, 0, 0, 0, 0]);
+  const[column1, setcolumn1] = useState([2, 0, 0, 0, 0, 0]);
+  const[column2, setcolumn2] = useState([2, 0, 0, 0, 0, 0]);
+  const[column3, setcolumn3] = useState([2, 0, 0, 0, 0, 0]);
+  const[column4, setcolumn4] = useState([2, 0, 0, 0, 0, 0]);
+  const[column5, setcolumn5] = useState([2, 0, 0, 0, 0, 0]);
+  const[column6, setcolumn6] = useState([1, 0, 0, 0, 0, 0]);
+  const[wholeTable, setWholeTable] = useState([column0, column1, column2, column3, column4, column5, column6]);
+
+  //Set who's turn it is and the timer
+  const[whosTurnIsIt, setWhosTurnItIs] = useState("red");
+  const[turnCounter, setTurnCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTurnCounter(prevCount => prevCount + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  function addToken(column) {
+    setWholeTable([column0, column1, column2, column3, column4, column5, column6]);
+    console.log(wholeTable);
+    if (whosTurnIsIt === "red") {
+      setWhosTurnItIs("yellow"); 
+    } else{
+      setWhosTurnItIs("red"); 
+    }
+    setTurnCounter(0);
+  }
+
+  //Moving the marker
+  const[markerX, setMarkerX] = useState("11%");
+  function moveMarker(amount) {
+    setMarkerX(amount);
+  }
+
+  //Scoring
+  const[playerOneScore, setPlayerOneScore] = useState(0);
+  const[playerTwoScore, setPlayerTwoScore] = useState(0);
+
   return (
     <div className="playArea">
       <div className="playButtonsContainer">
@@ -12,39 +55,79 @@ function Player() {
       <div className="playersBox">
         <div className="playersTextBubble">
           <h2>PLAYER 1</h2>
-          <p>12</p>
+          <p>{playerOneScore}</p>
           <img className='playerOneImage' src="./assets/images/player-one.svg" alt="Player 1 smiley face" />
         </div>
         <div className="playersTextBubble">
-          <h2>PLAYER 1</h2>
-          <p>12</p>
+          <h2>PLAYER 2</h2>
+          <p>{playerTwoScore}</p>
           <img className='playerTwoImage' src="./assets/images/player-two.svg" alt="Player 2 smiley face" />
         </div>
       </div>
 
       <div className="gameBoard">
         <img className='backImageLayer' src="/assets/images/board-layer-black-small.svg" alt="back of connect 4 board" />
-        <table>
+        <table className='gameTable'>
           <tr>
-            <td><img className="token red" src="/assets/images/counter-red-small.svg" alt="" /></td>
-            <td><img className="token red" src="/assets/images/counter-red-small.svg" alt="" /></td>
-            <td><img className="token red" src="/assets/images/counter-red-small.svg" alt="" /></td>
-            <td><img className="token red" src="/assets/images/counter-red-small.svg" alt="" /></td>
-            <td><img className="token red" src="/assets/images/counter-red-small.svg" alt="" /></td>
-            <td><img className="token red" src="/assets/images/counter-red-small.svg" alt="" /></td>
-            <td><img className="token red" src="/assets/images/counter-red-small.svg" alt="" /></td>
+            <td>{
+            wholeTable[0][0] === 0 ? 
+            null 
+            : wholeTable[0][0] === 1 ? 
+            <img className="token red" src="/assets/images/counter-red-small.svg" alt="" />
+            : <img className="token yellow" src="/assets/images/counter-yellow-small.svg" alt="" />
+            }</td>
+
+            <td>{
+            wholeTable[1][0] === 0 ? 
+            null 
+            : wholeTable[1][0] === 1 ? 
+            <img className="token red" src="/assets/images/counter-red-small.svg" alt="" />
+            : <img className="token yellow" src="/assets/images/counter-yellow-small.svg" alt="" />
+            }</td>
+
+            <td>{
+            wholeTable[2][0] === 0 ? 
+            null 
+            : wholeTable[2][0] === 1 ? 
+            <img className="token red" src="/assets/images/counter-red-small.svg" alt="" />
+            : <img className="token yellow" src="/assets/images/counter-yellow-small.svg" alt="" />
+            }</td>
+
+            <td>{
+            column3[0] === 0 ? 
+            null 
+            : column3[0] === 1 ? 
+            <img className="token red" src="/assets/images/counter-red-small.svg" alt="" />
+            : <img className="token yellow" src="/assets/images/counter-yellow-small.svg" alt="" />
+            }</td>
+
+            <td>{
+            column4[0] === 0 ? 
+            null 
+            : column4[0] === 1 ? 
+            <img className="token red" src="/assets/images/counter-red-small.svg" alt="" />
+            : <img className="token yellow" src="/assets/images/counter-yellow-small.svg" alt="" />
+            }</td>
+            
+            <td>{
+            column5[0] === 0 ? 
+            null 
+            : column5[0] === 1 ? 
+            <img className="token red" src="/assets/images/counter-red-small.svg" alt="" />
+            : <img className="token yellow" src="/assets/images/counter-yellow-small.svg" alt="" />
+            }</td>
+            
+            <td>{
+            column6[0] === 0 ? 
+            null 
+            : column6[0] === 1 ? 
+            <img className="token red" src="/assets/images/counter-red-small.svg" alt="" />
+            : <img className="token yellow" src="/assets/images/counter-yellow-small.svg" alt="" />
+            }</td>
+            
           </tr>
           <tr>
-            <td><img className="token red" src="/assets/images/counter-red-small.svg" alt="" /></td>
             <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td><img className="token red" src="/assets/images/counter-red-small.svg" alt="" /></td>
             <td></td>
             <td></td>
             <td></td>
@@ -53,16 +136,7 @@ function Player() {
             <td></td>
           </tr>
           <tr>
-            <td><img className="token red" src="/assets/images/counter-red-small.svg" alt="" /></td>
             <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td><img className="token red" src="/assets/images/counter-red-small.svg" alt="" /></td>
             <td></td>
             <td></td>
             <td></td>
@@ -71,7 +145,25 @@ function Player() {
             <td></td>
           </tr>
           <tr>
-            <td><img className="token red" src="/assets/images/counter-red-small.svg" alt="" /></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td></td>
             <td></td>
             <td></td>
             <td></td>
@@ -82,10 +174,22 @@ function Player() {
         </table>
         <img className="frontImageLayer" src="/assets/images/board-layer-white-small.svg" alt="Front of connect 4 board"/>
         
-        <div className="playerTurnBox">
-            <h3>PLAYER 1'S TURN</h3>
-            <p>15s</p>
+        <div className={"playerTurnBox " + whosTurnIsIt}>
+            <h3>PLAYER {whosTurnIsIt === "red"? "1" : "2"}'S TURN</h3>
+            <p>{turnCounter}s</p>
         </div>
+        <table className='invisibleEventListener'>
+            <tr>
+            <img className={"markerImage " + whosTurnIsIt} src="/assets/images/marker-red.svg" alt="marker" style= {{left: markerX}}/>
+              <td onClick={() => addToken(0)} onMouseEnter={() => moveMarker("11%")}></td>
+              <td onClick={() => addToken(1)} onMouseEnter={() => moveMarker("24.5%")}></td>
+              <td onClick={() => addToken(2)} onMouseEnter={() => moveMarker("37.55%")}></td>
+              <td onClick={() => addToken(3)} onMouseEnter={() => moveMarker("50.5%")}></td>
+              <td onClick={() => addToken(4)} onMouseEnter={() => moveMarker("63.7%")}></td>
+              <td onClick={() => addToken(5)} onMouseEnter={() => moveMarker("77%")}></td>
+              <td onClick={() => addToken(6)} onMouseEnter={() => moveMarker("90%")}></td>
+            </tr>
+        </table>
       </div>
     </div>
   )
